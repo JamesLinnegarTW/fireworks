2ff685f62df9e9d8a6ca6cb6afd5d3a4e88b6089
James Linnegar <jlinnega@thoughtworks.com>
2013-12-17 13:34:12 +0000

    project restructuring...

---
+++ public/js/pongcontrol.js
@@ -0,0 +1,79 @@
+// Client
+$(function(){
+    $("#color").spectrum({
+        color: "#f00"
+    });
+
+    $("#color2").spectrum({
+        color: "#f00"
+    });
+
+    var socket = io.connect(
+        'http://'+window.location.hostname+':9000/'
+    );
+
+    socket.on('connect', function () {
+        var x, y, direction = {x:0,y:0};
+        var drawable = document.getElementById('draw');
+
+        drawable.addEventListener('touchstart', function (e) {   
+
+            e.preventDefault(); 
+            var data = {f:[]};
+
+            for(var i= 0; i< e.changedTouches.length; i++){
+                data.f.push({
+                    'y':(e.touches[i].clientY/  window.innerHeight),
+                    'x':(e.touches[i].clientX/  window.innerWidth),
+                    'colors':[ $("#color").spectrum("get").toRgb(),
+                               $("#color2").spectrum("get").toRgb()
+                            ],
+                    'bang': $('#bang').val()/10
+                });
+            }
+            socket.emit('m', data);  
+
+        }, false);
+
+        drawable.addEventListener('touchmove', function (e) { 	
+
+			e.preventDefault(); 
+
+
+            var data = {f:[]};
+
+            for(var i= 0; i< e.changedTouches.length; i++){
+                data.f.push({
+                    'y':(e.touches[i].clientY/  window.innerHeight),
+                    'x':(e.touches[i].clientX/  window.innerWidth),
+                    'colors':[ $("#color").spectrum("get").toRgb(),
+                               $("#color2").spectrum("get").toRgb()
+                            ],
+                    'bang': $('#bang').val()/10
+                });
+            }
+            socket.emit('m', data);          
+
+		}, false);
+
+
+        drawable.addEventListener('touchend', function (e) {  
+            var data = {f:[]};
+
+            for(var i= 0; i< e.changedTouches.length; i++){
+                data.f.push({
+                    'y':(e.changedTouches[i].clientY/  window.innerHeight),
+                    'x':(e.changedTouches[i].clientX/  window.innerWidth),
+                    'colors':[ $("#color").spectrum("get").toRgb(),
+                               $("#color2").spectrum("get").toRgb()
+                            ],
+                    'bang': $('#bang').val()/10
+                });
+            }
+            socket.emit('b', data);
+        }, false);        
+    
+
+	});
+
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+var io = require('socket.io').listen(9000);
+var clients = [];
+var static = require('node-static');
+var file = new static.Server('./public');
+
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
+
+
+
+io.sockets.on('connection', function (socket) {
+    socket.on('v', function(data){
+        clients.push(socket);
+    });
+
+    socket.on('m', function(data) {
+        for(var i= 0; i < clients.length; i++) {
+            clients[i].emit('m', data);
+        }
+    });
+
+
+    socket.on('b', function(data) {
+        for(var i= 0; i < clients.length; i++) {
+            clients[i].emit('b', data);
+        }
+
+    });
+
+	socket.on('disconnect', function(){
+        for(var i= 0; i < clients.length; i++) {
+            if(clients[i].id == socket.id){
+                clients.splice(i,1);
+                break;                
+            }
+        }
+	});
+
+});
