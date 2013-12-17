7c30fd865f60c158bad23cfabee8ef1cb6dc85ae
James Linnegar <jlinnega@thoughtworks.com>
2013-12-17 14:00:10 +0000

    some more port checks

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var io = require('socket.io').listen(9000);
+var io = require('socket.io').listen(process.env.PORT || 8080);
 var clients = [];
 var static = require('node-static');
 var file = new static.Server('./public');
