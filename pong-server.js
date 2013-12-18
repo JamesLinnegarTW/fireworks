a76f93d98713f4480787d5170d46268e821962cf
James Linnegar <jlinnega@thoughtworks.com>
2013-12-18 19:39:06 +0000

    reset ball function and allow single player

--- pong-server.js
+++ pong-server.js
@@ -20,8 +20,8 @@ var paddles = [];
 var static = require('node-static');
 var file = new static.Server('./public');
 var ball = {x: 0.5,y:0.0};
-var speedY = 0.010;
-var speedX = -0.010;
+var speedY = 0.005;
+var speedX = -0.005;
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
@@ -36,6 +36,11 @@ function randomColor(){
         b: Math.round((Math.random() * 255))
     }
 }
+
+function noOfPlayers(){
+    return Object.keys(paddles).length;
+}
+
 io.sockets.on('connection', function (socket) {
     console.log("hello");
     clients.push(socket);
@@ -61,7 +66,7 @@ io.sockets.on('connection', function (socket) {
         var color = randomColor();
         socket.emit('color',color);
         console.log(paddles);
-        var newPaddle = {u: new Date(), y:0.5, side: ((Object.keys(paddles).length) % 2), color:color};
+        var newPaddle = {u: new Date(), y:0.5, side: (noOfPlayers() % 2), color:color};
         console.log(newPaddle);
         paddles[socket.id] = newPaddle;
 
@@ -101,9 +106,14 @@ io.sockets.on('connection', function (socket) {
 });
 
 function collisionDetect(side){
-
    var keys = Object.keys(paddles);
    var paddle;
+   
+
+    if(side == 1 && noOfPlayers() == 1){
+        return paddles[keys[0]].color;
+    }
+
     for(var i = 0; i < keys.length; i++){
         paddle = paddles[keys[i]];
 
@@ -116,64 +126,71 @@ function collisionDetect(side){
     return false;
 }
 
+function resetBall(){
+    speedX = 0.001;
+
+    ball.x = 0.5;
+    ball.y = Math.random();
+}
+
 setInterval(function(){
-    ball.x = ball.x + speedX;
-    ball.y = ball.y + speedY;
 
-    if(ball.y < 0) {
-        speedY = -speedY;
-    }
+    if(noOfPlayers() >= 1) {
+        ball.x = ball.x + speedX;
+        ball.y = ball.y + speedY;
 
-    if(ball.y > 1){
-        speedY = -speedY;
-    }
+        if(ball.y < 0) {
+            speedY = -speedY;
+        }
 
-    if(ball.x <= 0){
-        var collision = collisionDetect(0);
+        if(ball.y > 1){
+            speedY = -speedY;
+        }
 
-        if(collision){
-            speedX = (-speedX) + 0.005;
-            for(var i= 0; i < clients.length; i++) {
-                clients[i].emit('c', collision);
-            }  
-        } else {
-                       
-            for(var i= 0; i < clients.length; i++) {
-                clients[i].emit('b', {x:ball.x, y:ball.y});
+        if(ball.x <= 0){
+            var collision = collisionDetect(0);
+
+            if(collision){
+                speedX = (-speedX) + 0.001;
+                ball.x = 0.001;
+                for(var i= 0; i < clients.length; i++) {
+                    clients[i].emit('c', collision);
+                }  
+            } else {
+                           
+                for(var i= 0; i < clients.length; i++) {
+                    clients[i].emit('b', {x:ball.x, y:ball.y});
+                }
+
+                resetBall();
             }
-
-            speedX = 0.010;
             
-            if(Math.random() > 0.5) speedX = -speedX;
-
-            ball.x = 0.5;
-            ball.y = Math.random();
         }
         
-    }
-    
-    if(ball.x >= 1) {
+        if(ball.x >= 1) {
 
-        var collision = collisionDetect(1);
+            var collision = collisionDetect(1);
 
-        if(collision){
-            speedX = (-speedX) * 1.01;
-            for(var i= 0; i < clients.length; i++) {
-                clients[i].emit('c', collision);
-            }  
-        } else {
-                       
-            for(var i= 0; i < clients.length; i++) {
-                clients[i].emit('b', {x:ball.x, y:ball.y});
+            if(collision){
+                speedX = (-speedX) + 0.001;
+                ball.x = 1 - 0.001;
+                for(var i= 0; i < clients.length; i++) {
+                    clients[i].emit('c', collision);
+                }  
+            } else {
+                           
+                for(var i= 0; i < clients.length; i++) {
+                    clients[i].emit('b', {x:ball.x, y:ball.y});
+                }
+                speedX = -0.010;
+                ball.x = 0.5;
+                ball.y = Math.random();
             }
-            speedX = -0.010;
-            ball.x = 0.5;
-            ball.y = Math.random();
+
         }
 
-    }
-
-    for(var i= 0; i < clients.length; i++) {
-        clients[i].emit('ball', ball);
+        for(var i= 0; i < clients.length; i++) {
+            clients[i].emit('ball', ball);
+        }
     }
 },10);
